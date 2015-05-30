cfb3bfd8e737673caa4a85aaf5e10d8265c9cf31
Marcos Serpa <marcosserpa@gmail.com>
2015-05-30 13:15:01 -0300

    Fixes name field in aliments table to disallow null.

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20150530032208) do
   enable_extension "plpgsql"
 
   create_table "aliments", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",        null: false
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
