ffa16fa41e8cf249d9a09f8077d576c777d1e890
Marcos Serpa <marcosserpa@gmail.com>
2015-06-06 17:11:48 -0300

    Adds better_errors gem Adds tests to aliment model

--- app/models/aliment.rb
+++ app/models/aliment.rb
@@ -1,12 +1,14 @@
 class Aliment < ActiveRecord::Base
 
-  has_one :proximate
-  has_one :mineral
-  has_one :vitamin
-  has_one :lipid
-  has_one :amino_acid
-  has_one :other
+  has_one :proximate, dependent: :destroy
+  has_one :mineral, dependent: :destroy
+  has_one :vitamin, dependent: :destroy
+  has_one :lipid, dependent: :destroy
+  has_one :amino_acid, dependent: :destroy
+  has_one :other, dependent: :destroy
 
+  # Ensures that, when trying to save aliment, validates belonged models too
   validates_associated :proximate, :mineral, :vitamin, :lipid, :amino_acid, :other
+  validates :name, presence: true
 
 end
