122c8217a0f80b3b250c34524b3b3a010658fb6b
Marcos Serpa <marcosserpa@gmail.com>
2016-09-25 12:10:42 -0300

    Adds purecss to layout

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # yummy
 
-## to run
+#### to run
 
 1) initialize elasticsearch (at the environment, aliased as 'elastic')
 2) open rails console
 3) run Aliment.reindex (all the time some index has changed or some insertion on the database has been made, this needs to be run)
+
+#### styles
+the application is using (http://purecss.io/) library version 0.6.0.
+
+#### to update the database
+we need to run the task:
+
+```shell
+rake integrate:update_aliments
+```
--- app/controllers/aliments_controller.rb
+++ app/controllers/aliments_controller.rb
@@ -3,17 +3,23 @@ class AlimentsController < ApplicationController
   autocomplete :aliment, :name
 
   def index
-    @aliments = if (params[:aliment].present? && !params[:aliment][:name].blank?)
-      Aliment.search(params[:aliment][:name], operator: 'or', page: params[:page], per_page: 20, fields: [
-        { name: :word_start },
-        { food_group: :word_start }
-      ])
-    else
-      Aliment.search('*', page: params[:page], per_page: 20)
-    end
+    binding.pry if params[:id].present?
+    # if params[:id].present?
+    #   @aliment = Aliment.find params[:id]
+    # else
+      @aliments = if (params[:aliment].present? && !params[:aliment][:name].blank?)
+        Aliment.search(params[:aliment][:name], operator: 'or', page: params[:page], per_page: 20, fields: [
+          { name: :word_start },
+          { food_group: :word_start }
+        ])
+      else
+        Aliment.search('*', page: params[:page], per_page: 20)
+      end
+    # end
   end
 
   def show
+    binding.pry
     @aliment = Aliment.find params[:id]
   end
 
